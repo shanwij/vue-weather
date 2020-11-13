3a8c0b527db54aed401f7be4924aacc2ae1ae034
Lakshan Wijewardana <48085984+shanwij@users.noreply.github.com>
2020-11-13 10:38:35 +0530

    Add vue weather app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VuePlaceAutocomplete from 'vue-place-autocomplete';
+
+Vue.use(VuePlaceAutocomplete);
 
 Vue.config.productionTip = false
 
